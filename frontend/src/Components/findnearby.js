e8a87fc35001b05f521c860ce13b34b1189b652b
dalvin81 <dalvin@usc.edu>
2024-05-26 19:05:19 -0700

    findnearby.js updated for API key

--- frontend/src/Components/findnearby.js
+++ frontend/src/Components/findnearby.js
@@ -9,7 +9,7 @@ export default function FindNearby() {
 
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = INSERT GOOGLE MAP API KEY;
+    script.src = "https://maps.googleapis.com/maps/api/js?key=YOURAPIKEY&callback=initMap&libraries=places";
     script.async = true;
     script.onload = initMap;
     document.body.appendChild(script);
